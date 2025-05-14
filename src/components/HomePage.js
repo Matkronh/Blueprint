94c397562bb5d19908714f67a8de9276d77161b2
Mats Kronheim <mats.kodehode@gmail.com>
2025-05-14 19:38:32 +0200

    dsa

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Helmet } from 'react-helmet';
 import './HomePage.css';
 
 function HomePage() {
   return (
+
+    
     <div className="home-container">
+        <Helmet>
+        <script>
+          {`
+            var MemberSpace = window.MemberSpace || {"subdomain":"blueprint2"};
+            (function(d){
+              var s = d.createElement("script");
+              s.src = "https://cdn.memberspace.com/scripts/widgets.js";
+              var e = d.getElementsByTagName("script")[0];
+              e.parentNode.insertBefore(s,e);
+            }(document));
+          `}
+        </script>
+      </Helmet>
       <header className="header">
         <h1>Welcome to Our Professional Platform</h1>
         <p>Your success is our priority</p>
